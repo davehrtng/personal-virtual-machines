d43989ee250ae81d22a2cf0528391d7082cd493a
davehrtng <david.e.harting@gmail.com>
2016-07-16 11:13:55 -0400

    remove old bash script for rails dev environment

--- rails/install.sh
+++
@@ -1,8 +0,0 @@
-# We install Ruby and Rails using Ruby Version Manager
-# key stuff below is pre-req for RVM install
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash
-
-# TODO SQLite 3 database for easy storage for rails apps
-
-# TODO PostgreSQL for "real" storage for rails apps
